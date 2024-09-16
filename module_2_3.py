ba2482f2b0a838b3a50dc90d7ceea32fce94b5ce
Gr4ch1337 <sneG4ee@mail.ru>
2024-09-16 15:38:00 +0300

    module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,1 +1,12 @@
-3
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+x = my_list[0]
+if x == 0:
+    del my_list[0]
+    x = my_list[0]
+while x >= 0 and len(my_list) > 0:
+    print(x)
+    del my_list[0]
+    if len(my_list) > 0:
+        x = my_list[0]
+    else:
+        break
